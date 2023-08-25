e3643796bedcababe9a40a539794ac52ce2c934f
Evan Hands <handsonevanoff@gmail.com>
2023-08-25 01:22:36 -0700

    test code removed for assertEqual and head.js file created

--- head.js
+++ head.js
@@ -1,5 +1,19 @@
 
-// A "True or False" comparison function
+/**
+ *  head gives the first element of an array.
+ * @param {array} singleArray Is the array given.
+ * @returns {element} The first element of singleArray.
+ */
+const head = function(singleArray) {
+  return singleArray[0];
+};
+
+/**
+ * assertEqual checks for the strict equality of two variables and gives a flashy Passed: or Failed: output.
+ * @param {variable} actual Is compared with expected.
+ * @param {variable} expected Is compared with actual.
+ * @returns {string} Notifies of pass or fail.
+ */
 const assertEqual = function(actual, expected) {
 
   // Emojis for success to make the output more readable
