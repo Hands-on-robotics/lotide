67f677f3484a8d182fdaeed62503f89b6c7c69d3
Evan Hands <handsonevanoff@gmail.com>
2023-09-13 20:23:44 -0700

    middleTest.js and tailTest.js refactored to use deepEqual instead of strictEqual

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,21 +1,19 @@
 // middleTest.js
 
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
-//const assertArraysEqual = require('../assertArraysEqual');
 const assert = require('chai').assert;
 
 describe("#middle", () => {
   it("Should return [2, 3] for [1, 2, 3, 4]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]), true);
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
   it("Should return [2] for [1, 2, 3]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2, 3]), [2]), true);
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
   it("Should return an empty array for [1, 2]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2]), []), true);
+    assert.deepEqual(middle([1, 2]), []);
   });
   it("Should return an empty array for [1]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1]), []), true);
+    assert.deepEqual(middle([1]), []);
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -6,9 +6,12 @@ const assert = require('chai').assert;
 
 describe("#tail", () => {
   it("Should return est for best", () => {
-    assert.strictEqual(tail("best"), "est");
+    assert.deepEqual(tail("best"), "est");
   });
   it("Should return 't' for 'st'", () => {
-    assert.strictEqual(tail("st"), "t");
+    assert.deepEqual(tail("st"), "t");
+  });
+  it("Should return [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
   });
 });
